e2d09fa8165411ef7e1d30c71f7527eb83d27d58
Asel <asel.alimbaeva.kk@gmail.com>
2022-01-31 15:36:45 +0300

    added style

---
+++ src/showcase.js
@@ -0,0 +1,19 @@
+import "./style/style.scss";
+
+const $showcase = document.querySelector('.showcase');
+
+const renderGoodsItem = ({ title, price }) => {
+  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodsList = (list) => {
+  let goodsList = list.map(
+    (item) => {
+      return renderGoodsItem(item)
+    }
+  ).join('');
+
+  $showcase.insertAdjacentHTML('beforeend', goodsList);
+}
+
+export default renderGoodsList
